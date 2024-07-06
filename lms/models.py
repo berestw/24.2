a0017cff9f6c4b589ae144b22d673c6f1f2bc483
berestw <paladin.berestw@yandex.ru>
2024-07-06 19:11:50 +0300

    25.2

--- lms/models.py
+++ lms/models.py
@@ -6,9 +6,9 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Course(models.Model):
-    title = models.CharField(max_length=150, verbose_name='название')
+    title = models.CharField(max_length=150, verbose_name='название', **NULLABLE)
     image = models.ImageField(upload_to='course', verbose_name='изображение', **NULLABLE)
-    content = models.TextField(verbose_name='описание')
+    content = models.TextField(verbose_name='описание', **NULLABLE)
     owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='владелец',
                               **NULLABLE)
 
@@ -35,4 +35,16 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
-        
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='подписчики',
+                             **NULLABLE)
+    course = models.ForeignKey(Course, verbose_name='курсы', on_delete=models.CASCADE, **NULLABLE)
+
+    def __str__(self):
+        return f'{self.user} {self.course}'
+
+    class Meta:
+        verbose_name = 'подписчик'
+        verbose_name_plural = 'подписчики'
